b2b3a80399b4c471c1f37ea37e65504d3b21dc4f
WEIIO <xiao.wei@danone.com>
2021-02-08 21:50:10 +0100

    add growth_list_direct_func

--- src/firm_input.py
+++ src/firm_input.py
@@ -1,11 +1,45 @@
 
 
 
-def growth_list():
+def growth_list_direct_func(r_gr_next, r_gr_high, length_high_growth, length_converge, r_riskfree,\
+                            flag_gr_terminal_direct, r_gr_terminal_direct):
+    assert length_converge < length_high_growth, "the converge period should be less than the high growth period"
+    gr_list = list()
+
+    if flag_gr_terminal_direct:
+        r_gr_terminal = r_gr_terminal_direct
+    else:
+        r_gr_terminal = r_riskfree
+
+    for i in range(1, length_high_growth+1):
+        if i == 1:
+            gr_list.append(r_gr_next)
+        elif i <= length_converge:
+            gr_list.append(r_gr_high)
+        else:
+            r_gr = r_gr_high-((r_gr_high-r_gr_terminal)/(length_high_growth-length_converge))*(i-length_converge)
+            gr_list.append(r_gr)
+    return gr_list
+
+
+def margin_list_func():
     pass
 
-def margin_list():
+def sales_to_capital_list_func():
     pass
 
-def sales_to_capital_list():
-    pass
+
+
+
+r_gr_next = 0.02
+r_gr_high = 0.02
+length_high_growth = 10
+length_converge = 5
+r_riskfree = 0.0111
+flag_gr_terminal_direct = False
+r_gr_terminal_direct = 0.02
+
+test = growth_list_direct_func(r_gr_next, r_gr_high, length_high_growth, length_converge, r_riskfree,\
+                            flag_gr_terminal_direct, r_gr_terminal_direct)
+
+1
---
+++ test/test_firm_input.py
@@ -0,0 +1,20 @@
+import unittest
+
+from src.firm_input import growth_list_direct_func
+
+class TestFirmInput(unittest.TestCase):
+    def test_growth_list_direct_func(self):
+        """
+        :return:
+        """
+        # test case 1
+        r_gr_next = 0.02
+        r_gr_high = 0.02
+        length_high_growth = 10
+        length_coverge = 5
+        r_riskfree = 0.0111
+        flag_gr_terminal_direct = False
+        r_gr_terminal_direct = 0.02
+        result = growth_list_direct_func(r_gr_next, r_gr_high, length_high_growth, length_coverge, r_riskfree,\
+                                         flag_gr_terminal_direct, r_gr_terminal_direct)
+        self.assertEqual(result, [0.02, 0.02, 0.02, 0.02, 0.02, 0.01822, 0.01644, 0.014660000000000001, 0.01288, 0.0111])
