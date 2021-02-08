91178be3c939d270ab438c701f97e170f8daf13e
WEIIO <xiao.wei@danone.com>
2021-02-08 22:03:42 +0100

    change to length_high_growth_stable

--- test/test_firm_input.py
+++ test/test_firm_input.py
@@ -11,10 +11,10 @@ class TestFirmInput(unittest.TestCase):
         r_gr_next = 0.02
         r_gr_high = 0.02
         length_high_growth = 10
-        length_coverge = 5
+        length_high_growth_stable = 5
         r_riskfree = 0.0111
         flag_gr_terminal_direct = False
         r_gr_terminal_direct = 0.02
-        result = growth_list_direct_func(r_gr_next, r_gr_high, length_high_growth, length_coverge, r_riskfree,\
-                                         flag_gr_terminal_direct, r_gr_terminal_direct)
+        result = growth_list_direct_func(r_gr_next, r_gr_high, length_high_growth, length_high_growth_stable,\
+                                         r_riskfree, flag_gr_terminal_direct, r_gr_terminal_direct)
         self.assertEqual(result, [0.02, 0.02, 0.02, 0.02, 0.02, 0.01822, 0.01644, 0.014660000000000001, 0.01288, 0.0111])
