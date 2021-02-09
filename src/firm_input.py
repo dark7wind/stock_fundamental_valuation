d2a017737f10d1397d320f9511ac1cd5cb552a7c
WEIIO <xiao.wei@danone.com>
2021-02-09 22:43:08 +0100

    'firm_input'

--- src/firm_input.py
+++ src/firm_input.py
@@ -3,6 +3,16 @@
 
 def growth_list_direct_func(r_gr_next, r_gr_high, length_high_growth, length_high_growth_stable, r_riskfree,\
                             flag_gr_terminal_direct, r_gr_terminal_direct):
+    """
+    :param r_gr_next: growth rate next year
+    :param r_gr_high: growth rate in high growth period
+    :param length_high_growth: length of high growth period
+    :param length_high_growth_stable: length of high and stable growth period
+    :param r_riskfree:
+    :param flag_gr_terminal_direct: flag if direct input terminal growth rate
+    :param r_gr_terminal_direct:
+    :return:
+    """
     assert length_high_growth_stable < length_high_growth, "the converge period should be less than the high growth period"
     gr_list = list()
 
@@ -23,24 +33,57 @@ def growth_list_direct_func(r_gr_next, r_gr_high, length_high_growth, length_hig
     return gr_list
 
 
-def margin_list_func():
+def margin_list_direct_func(margin_next_year, margin_target, converge_year, length_high_growth):
+    """
+    :param margin_next_year: margin in next year
+    :param margin_target: target margin
+    :param converge_year: the length of period to converge to target margin
+    :param length_high_growth: length of high growth period
+    :return:
+    """
+    margin_list = list()
+    for i in range(1, length_high_growth+1):
+        if i == 1:
+            margin = margin_next_year
+            margin_list.append(margin)
+        if i >1 and i <= converge_year:
+            margin = margin_target-((margin_target-margin_next_year)/converge_year)*(converge_year-i)
+            margin_list.append(margin)
+        if i > converge_year:
+            margin = margin_target
+            margin_list.append(margin)
+    return margin_list
+
+def sales_to_capital_list_func(revenue, invested_capital, industry_us, industry_global, length_high_growth,\
+                               flag='company'):
+    if flag == "company":
+        sales_to_capital = revenue/invested_capital
+        return [sales_to_capital]*length_high_growth
+
+    elif flag == "industry_us":
+        sales_to_capital = industry_us
+        return [sales_to_capital] * length_high_growth
+
+    elif flag == "industry_global":
+        sales_to_capital = industry_global
+        return [sales_to_capital] * length_high_growth
+
+    else:
+        print('the flag should be one of the following: 1. company, 2. industry_us, 3. industry_global')
+
+
+def tax_rate_list_func(net_income_before_tax, tax, r_marginal_tax, flag_terminal_tax,):
     pass
 
-def sales_to_capital_list_func():
-    pass
 
-
-
-
-r_gr_next = 0.02
-r_gr_high = 0.02
+flag = "company"
+revenue = 43185
+invested_capital = 26037
 length_high_growth = 10
-length_high_growth_stable = 5
-r_riskfree = 0.0111
-flag_gr_terminal_direct = False
-r_gr_terminal_direct = 0.02
+industry_us = 1.36
+industry_global = 1.66
 
-test = growth_list_direct_func(r_gr_next, r_gr_high, length_high_growth, length_high_growth_stable, r_riskfree,\
-                            flag_gr_terminal_direct, r_gr_terminal_direct)
+test = sales_to_capital_list_func(revenue, invested_capital, industry_us, industry_global, length_high_growth,\
+                                  flag='company')
 
 1
