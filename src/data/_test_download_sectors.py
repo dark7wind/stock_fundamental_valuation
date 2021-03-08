a1031cd579f73c170398b1a45ea771a965fe4e0f
dark7wind <xiaowei8898@gmail.com>
2021-03-08 23:07:18 +0100

    update stock info wip

--- src/data/_test_download_sectors.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import MySQLdb as mdb
-import os
-import yaml
-import datetime
-
-tickerdata = yf.Ticker('TYL') #the tickersymbol for Tesla
-print (tickerdata.info['industry'])
-1
