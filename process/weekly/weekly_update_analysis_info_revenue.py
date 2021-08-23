38624565bff857c3212b2bd3fa4314672b4579a1
dark7wind <xiaowei8898@gmail.com>
2021-08-23 19:50:06 +0200

    dashboard download

--- process/weekly/weekly_update_analysis_info_revenue.py
+++ process/weekly/weekly_update_analysis_info_revenue.py
@@ -1,4 +1,9 @@
 from src.database.update_table_analysis_info_revenue_sp500 import insert_updated_analysis_info_revenue_data_into_db
 
-insert_updated_analysis_info_revenue_data_into_db()
-print('complete analysis info revenue sp500')
+def weekly_update_analysis_info_revenue():
+    insert_updated_analysis_info_revenue_data_into_db()
+    print('complete analysis info revenue sp500')
+
+
+if __name__ == '__main__':
+    weekly_update_analysis_info_revenue()
