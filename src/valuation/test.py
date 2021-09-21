39a7a468717f9fe0949ff79c2a7b35154e5b3043
WEIIO <xiao.wei@danone.com>
2021-09-21 09:43:21 +0200

    gitignore

--- src/valuation/test.py
+++ src/valuation/test.py
@@ -1,3 +1,6 @@
+# import sys
+# sys.path.append('c:\\Users\\weiio\\Documents\\ds_project\\stock_fundamental_valuation')
+import definitions
 
 flag_cost_capital_terminal_override = False
 flag_risk_free_terminal_override = False
